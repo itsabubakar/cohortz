474bfeb4b387008233c1623c4ccb6a667774628f
Hamid-Usman <abdulhamidusman218gmail.com>
2025-11-11 09:33:37 -0800

    fixed page components

--- app/(auth)/email-confirmation.tsx
+++ app/(auth)/email-confirmation.tsx
@@ -80,8 +80,8 @@ const EmailConfirmation = (props: Props) => {
         {/* <Text>We'll send you a quick email to confirm your address.</Text> */}
         
         
-        <View style={{gap: 1, marginTop: 2}}>
-          <Text>Password</Text>
+        <View style={{gap: 2, marginTop: 8}}>
+          <Text>Email</Text>
           <TextInput 
             style={styles.input}
             autoCapitalize="none"
@@ -90,7 +90,7 @@ const EmailConfirmation = (props: Props) => {
             onChangeText={setEmail}
           />
         </View>
-        <View style={{gap: 1, marginTop: 2}}>
+        <View style={{gap: 2, marginTop: 8}}>
           <Text>Password</Text>
           <TextInput 
             style={styles.input}
@@ -101,7 +101,7 @@ const EmailConfirmation = (props: Props) => {
             placeholder="Confirm Password"
           />
         </View>
-        <View style={{gap: 1, marginTop: 2}}>
+        <View style={{gap: 2, marginTop: 8}}>
           <Text>Confirm Password</Text>
           <TextInput 
             style={styles.input}
